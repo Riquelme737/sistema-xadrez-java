abe79b4aa0176af411eba0094ee38cbcf5c845be
Riquelme O. Santos <7riquelmesantos2006@gmail.com>
2024-10-23 18:19:43 -0300

    Primeira classe: Posição

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,7 +1,11 @@
 package aplicacao;
 
+import jogotabuleiro.Posicao;
+
 public class Programa {
     public static void main(String[] args) {
 
+        Posicao posicao = new Posicao(12, 3);
+        System.out.println(posicao);
     }
 }
